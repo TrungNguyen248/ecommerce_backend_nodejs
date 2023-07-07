6877a689cb6304c8d628798adf9560b5b8c7a20c
TrungNguyen248 <nguyentrungnguyen12a3@gmail.com>
2023-07-07 08:18:47 +0700

    init package

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+const compression = require('compression')
 const express = require('express')
+const {default : helmet} = require('helmet')
+const morgan = require('morgan')
 const app = express()
 
+//init middlewares
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+
+//init db
+
+//init routes
+app.get('/', (req, res, next) => {
+    const strCompress = 'hello there'
+
+    return res.status(200).json({
+        message: 'Welcome welcome',
+        metadata: strCompress.repeat(10000)
+    })
+})
+
+
+//handling error
+
+module.exports = app
